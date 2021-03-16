0d27e79b53830e60889c65269cbbfc0c3016f261
MarcSkovMadsen <marc.skov.madsen@gmail.com>
2021-03-16 05:14:37 +0000

    first iteration based on Custom Externsions Guide

--- panel/models/__init__.py
+++ panel/models/__init__.py
@@ -12,6 +12,7 @@ from .location import Location # noqa
 from .markup import JSON, HTML # noqa
 from .state import State # noqa
 from .tabulator import DataTabulator # noqa
+from .terminal import Terminal # noqa
 from .trend import TrendIndicator # noqa
 from .widgets import ( # noqa
     Audio, FileDownload, Player, Progress, SingleSelect, Video, VideoStream
